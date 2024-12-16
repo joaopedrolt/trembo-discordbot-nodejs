b49a2e12404535d6de9ae99b43616693a8632eff
João Pedro Lima Teixeira <Joo Teixeira@sindatadc.local>
2024-12-16 11:24:46 -0300

    New youtube playlist url fix

--- src/utils/urlTools/isYoutubePlaylist.js
+++ src/utils/urlTools/isYoutubePlaylist.js
@@ -1,7 +1,14 @@
 export default (url) => {
-  const parsedUrl = new URL(url);
-  return parsedUrl.hostname === "www.youtube.com" &&
-    parsedUrl.pathname === "/playlist"
-    ? true
-    : false;
+  try {
+    const parsedUrl = new URL(url);
+    if (parsedUrl.hostname === "www.youtube.com" || parsedUrl.hostname === "youtube.com") {
+      return (
+        (parsedUrl.pathname === "/playlist" && parsedUrl.searchParams.has("list")) || // Direct playlist URL
+        (parsedUrl.pathname === "/watch" && parsedUrl.searchParams.has("list"))      // Watch URL with a playlist
+      );
+    }
+    return false;
+  } catch (error) {
+    return false;
+  }
 };
